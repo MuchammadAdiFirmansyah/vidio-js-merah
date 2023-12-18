4df8500dae2214fdab77e5af8df0108936185c30
FirManz <muchammadadifirmansyah@gmail.com>
2023-12-18 08:48:31 +0700

    server 0.1

---
+++ JavaScript/js/logika.js
@@ -0,0 +1,31 @@
+let a = true;
+let b = false;
+let c = true;
+let d = false;
+
+// or
+// akan bernilai true jika salah satu true
+console.log(a || b);
+
+// and
+// akan bernilai false jika salah satu false
+console.log(a && b && c);
+
+let x = 8;
+let y = 7;
+
+console.log(x < y);
+
+console.log(x > y);
+
+console.log(x == y);
+
+console.log(x >= y);
+
+console.log(x <= y);
+
+let lima = "5";
+let limo = "5";
+
+// identik sama tipe dan isi
+console.log(lima === limo);
---
+++ JavaScript/js/operator.js
@@ -0,0 +1,59 @@
+let x = 2;
+
+x = -x;
+
+console.log(x);
+
+let p = 5,
+ l = 6,
+ t = 7,
+ volume;
+
+// p = 5;
+// l = 6;
+// t = 7;
+
+volume = p * l * t;
+
+console.log(volume);
+
+console.log(2 + 3);
+console.log(2 - 3);
+console.log(2 * 3);
+console.log(2 / 3);
+console.log(5 % 2);
+
+let kota = "sido" + "arjo";
+console.log(kota);
+
+console.log("1" + 5 + 6); 
+console.log(1 + 5 + "6"); 
+
+console.log(5 - "3"); 
+console.log("5" - 3); 
+
+console.log("5" / "3"); 
+console.log(5 / "3"); 
+console.log("5" / 3); 
+
+console.log(4 ** 2);
+
+let a, b, c;
+
+a = b = c = 3 + 3;
+
+console.log(b);
+
+let inc = 3;
+
+inc++;
+
+console.log(inc);
+
+dec--;
+
+console.log(dec);
+
+let coba = (2-1, 2+1, 5 + 2);
+
+console.log(coba); 
